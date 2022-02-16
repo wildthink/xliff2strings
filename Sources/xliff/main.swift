35a8077aba39be26de0d8b851aabfb9d5a81111a
Jason Jobe <jason.jobe@aboutobjects.com>
2022-02-16 15:06:25 -0500

    code cleanup

--- Sources/xliff/main.swift
+++ Sources/xliff/main.swift
@@ -3,72 +3,6 @@ import XMLCoder
 import ArgumentParser
 import xliff2strings
 
-//
-//  EchoCommand.swift
-//
-//  Created by Jason Jobe on 1/20/22.
-//
-extension Command {
-    struct XliffCmd: ParsableCommand {
-        static var configuration: CommandConfiguration {
-            .init(
-                commandName: "xliff2strings",
-                abstract: "echos command line arguments for testing"
-            )
-        }
-        
-        @Argument(help: "The input")
-        var input: [String]
-        
-        @Option(name: .shortAndLong, help: "The file to output to. Defaults to <stdout>.")
-        var output: String?
-        
-//        @Option(name: .shortAndLong, help: "An array of args")
-//        var argv: [String]
-//
-//        @Flag(name: .shortAndLong, help: "Flag check")
-//        var flag: Bool = false
-
-        func run() throws {
-            for item in input {
-                print("input -> \(item)")
-            }
-            if let output = output {
-                print ("output ->", output)
-            }
-            echoProcessInfo()
-        }
-        
-
-        func echoProcessInfo() {
-            let p = Process()
-            if let cwd = p.currentDirectoryURL?.path {
-                print("cwd", cwd)
-            }
-            if let ewd = p.executableURL?.path {
-                print("ewd", ewd)
-            }
-            if let env = p.environment?.enumerated() {
-                for (key, value) in env {
-                    print (key, value)
-                }
-            }
-        }
-    }
-}
-
-//let cli = CLI(singleCommand: XliffToStrings())
-//cli.helpCommand = nil // no options, no help needed
-//cli.helpFlag = nil
-
-//let cli = CLI(name: "xliff2strings",
-//              version: "0.1",
-//              description: "A command line tool to generate .strings and .stringdict files from .xliff file",
-//              commands: [XliffToStrings()])
-
-//exit(cli.go(with: ["-h"]))
-//cli.goAndExit()
-
 enum Command {}
 
 let BUILD_VERSION = "0.0.1"
@@ -82,10 +16,6 @@ extension Command {
                 abstract: "A program to create Localization string resources",
                 version: BUILD_VERSION,
                 subcommands: [
-                    Command.XliffCmd.self,
-//                    Command.PluralsCommand.self,
-//                    Command.Echo.self,
-//                    Command.Xcloc.self,
                 ]
             )
         }
@@ -128,7 +58,6 @@ extension Command {
             } else {
                 out = workingDir
             }
-//            let strings = xliff.getStringFiles(outFolder: out)
             let strings = try xliff.stringFiles(matching: filter, out: out)
 
             for st in strings {
